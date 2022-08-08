8dcbd033e9092642a471698da00f9c7427c1a35c
ardririy <hynnkgw27@gmail.com>
2022-08-09 01:06:35 +0900

    📜lets APG4b !

---
+++ APG4b/1-2.cc
@@ -0,0 +1,20 @@
+/*
+A君が書いたプログラム
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  cout << "いつも << 252 << endl;
+  cout << "AtCoderくん"　<< endl
+}
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  cout << "いつも" << 2525 << endl;
+  cout << "AtCoderくん" << endl;
+}
