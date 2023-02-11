3f7184144406dd45f1148beb42e17d79a6731cf8
ardririy <hynnkgw27@gmail.com>
2023-02-12 01:07:35 +0900

    vector_finderを追加

--- template.cc
+++ template.cc
@@ -1,5 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main() {}
+
+// vectorの中に期待する要素が含まれているかを返す
+int vector_finder(std::vector<int> vec, int number) {
+  auto itr = std::find(vec.begin(), vec.end(), number);
+  size_t index = std::distance(vec.begin(), itr);
+  if (index != vec.size()) {  // 発見できたとき
+    return 1;
+  } else {  // 発見できなかったとき
+    return 0;
+  }
 }
