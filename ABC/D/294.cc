54e231d55678573b00cc679d8bde5f95a7d00a08
ardririy <hynnkgw27@gmail.com>
2023-04-14 12:22:40 +0900

    D問題通った！！！！！！ウッヒョオオオアアア！！！！！！！！！！！！！！！！！！

--- ABC/D/294.cc
+++ ABC/D/294.cc
@@ -1,5 +1,3 @@
-// TLE + コーナーケースでWA
-
 #include <bits/stdc++.h>
 
 #include <map>
@@ -8,28 +6,29 @@
 #include <vector>
 using namespace std;
 
+#define PRINT(x) cout << x << endl;
+#define LOOP(i, n) for (int i = 0; i < n; i++)
+
 int main() {
     int n, q;
     cin >> n >> q;
 
-    set<int> wait;
-    set<int> called;
-
-    for (int i = 0; i < n; i++) wait.insert(i + 1);
-    int e;
-    int x;
-    for (int i = 0; i < q; i++) {
+    set<int> st;
+    int next = 1;
+    vector<int> ans(q);
+    int a_i = 0;
+    LOOP(i, q) {
+        int e;
         cin >> e;
-        if (e == 1) {
-            called.insert(*wait.begin());
-            wait.erase(wait.begin());
-        } else if (e == 2) {
-            cin >> x;
-            called.erase(x);
-        } else {
-            cout << *called.begin() << endl;
-            called.erase(called.begin());
-        }
+        if (e == 1)
+            st.insert(next++);
+        else if (e == 2) {
+            cin >> e;
+            st.erase(e);
+        } else
+            ans[a_i++] = *st.begin();
     }
+    LOOP(i, a_i) cout << ans[i] << endl;
+
     return 0;
 }
