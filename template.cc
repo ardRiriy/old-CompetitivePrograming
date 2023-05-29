9765f20b3a404e414e71ec540ec72fccedaf8192
ardririy <hynnkgw27@gmail.com>
2023-05-29 23:02:14 +0900

    テンプレートに色々追加した

--- template.cc
+++ template.cc
@@ -2,8 +2,16 @@
 
 #define int long long
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
+const int INF = 1e9;
 
 using namespace std;
+bool chmin(int &a, int b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
 
 signed main() {
     std::cout << std::fixed;
