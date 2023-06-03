e70e65b8064ba5b1478eaf6da8740d926eef496b
ardririy <hynnkgw27@gmail.com>
2023-06-03 11:35:59 +0900

    templateにchmaxを入れた

--- template.cc
+++ template.cc
@@ -2,7 +2,8 @@
 
 #define int long long
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
-const int INF = 1e9;
+const int INF = LLONG_MAX;
+const int N_INF = LLONG_MIN;
 
 using namespace std;
 bool chmin(int &a, int b) {
@@ -13,6 +14,13 @@ bool chmin(int &a, int b) {
     return false;
 }
 
+bool chmax(int &a, int b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
 signed main() {
     std::cout << std::fixed;
     std::cout << std::setprecision(20);
