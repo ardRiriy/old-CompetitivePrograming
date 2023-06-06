a211384b57304d4890fe74b3dbec298d46f6d72f
ardririy <hynnkgw27@gmail.com>
2023-06-06 14:43:09 +0900

    ufを自前実装した

--- template/unionfind.cpp
+++ template/unionfind.cpp
@@ -1,54 +1,30 @@
 class UnionFind {
-   public:
-    UnionFind() = default;
-
-    /// @brief Union-Find 木を構築します。
-    /// @param n 要素数
-    explicit UnionFind(size_t n) : m_parentsOrSize(n, -1) {}
-
-    /// @brief 頂点 i の root のインデックスを返します
-    /// @param i 調べる頂点のインデックス
-    /// @return 頂点 i の root のインデックス
-    int find(int i) {
-        if (m_parentsOrSize[i] < 0) {
-            return i;
-        }
-
-        // 経路圧縮
-        return (m_parentsOrSize[i] = find(m_parentsOrSize[i]));
-    }
-
-    /// @brief a のグループと b のグループを統合します。
-    /// @param a 一方のインデックス
-    /// @param b 他方のインデックス
-    void merge(int a, int b) {
-        a = find(a);
-        b = find(b);
-
-        if (a != b) {
-            // union by size (小さいほうが子になる）
-            if (-m_parentsOrSize[a] < -m_parentsOrSize[b]) {
-                std::swap(a, b);
-            }
-
-            m_parentsOrSize[a] += m_parentsOrSize[b];
-            m_parentsOrSize[b] = a;
-        }
-    }
-
-    /// @brief a と b が同じグループに属すかを返します。
-    /// @param a 一方のインデックス
-    /// @param b 他方のインデックス
-    /// @return a と b が同じグループに属す場合 true, それ以外の場合は false
-    bool connected(int a, int b) { return (find(a) == find(b)); }
-
-    /// @brief i が属するグループの要素数を返します。
-    /// @param i インデックス
-    /// @return i が属するグループの要素数
-    int size(int i) { return -m_parentsOrSize[find(i)]; }
-
    private:
-    // m_parentsOrSize[i] は i の 親,
-    // ただし root の場合は (-1 * そのグループに属する要素数)
-    std::vector<int> m_parentsOrSize;
-}; */
+    vector<int> uf;
+
+   public:
+    UnionFind(int size) : uf(size) {
+        for (int i = 0; i < size; i++) uf[i] = i;
+    }
+
+    int root(int n) {
+        if (uf[n] != n) uf[n] = root(uf[n]);
+        return uf[n];
+    }
+
+    bool connected(int a, int b) { return root(a) == root(b); }
+
+    void marge(int a, int b) {
+        int root_a = root(a);
+        int root_b = root(b);
+        if (root_a != root_b) uf[root_a] = root_b;
+    }
+
+    int size(int n) {
+        int cnt = 0;
+        int r = root(n);
+        for (int k : uf)
+            if (k == r) cnt++;
+        return cnt;
+    }
+};
