b8c4a2fe1db8bee219e97e5841b02932b971a85c
ardririy <hynnkgw27@gmail.com>
2023-08-01 22:15:17 +0900

    UFをmapにしたり，powerが繰り返し2乗法になったりした

--- template.cpp
+++ template.cpp
@@ -1,22 +1,23 @@
 #include <bits/stdc++.h>
 #define int long long
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define revrep(i, n) for (int i = (int)(n); i >= 0; i--)
+#define itrep(itr, stl) for(auto itr = stl.begin(); itr != stl.end(); itr++)
 #define print(x) cout << x << endl
 const int INF = LLONG_MAX;
 const int N_INF = LLONG_MIN;
 using namespace std;
 class UnionFind { 
-    private: vector<int> uf;
+    private: map<int, int> uf;
     public:
-    UnionFind(int size) : uf(size) { for (int i = 0; i < size; i++) uf[i] = -1; }
-    int root(int n) { if (uf[n] < 0) return n; else return uf[n] = root(uf[n]); }
+    int root(int n) { if (uf.find(n) == uf.end()) uf[n] = -1; if (uf[n] < 0) return n; else return uf[n] = root(uf[n]); }
     bool connected(int a, int b) { return root(a) == root(b); }
     void marge(int a, int b) { int root_a = root(a); int root_b = root(b); if (root_a != root_b) { if (uf[root_a] > uf[root_b]) swap(root_a, root_b); uf[root_a] += uf[root_b]; uf[root_b] = root_a; }}
     int size(int n) { return -uf[root(n)]; }
 };
 bool chmin(int &a, int b) { if (a > b) { a = b; return true; } return false; }
 bool chmax(int &a, int b) { if (a < b) { a = b; return true; } return false; }
-int power(int base, int exponent) {int result = 1;for (int i = 0; i < exponent; i++) result *= base; return result; }
+int power(int x, int n) { int result = 1; while(n > 0){ if((n & 1) == 1){ result *= x; } x *= x; n >>= 1; } return result; } /*x^nを計算*/
 int b_search(vector<int>& v, int k) { int ng = -1, ok = v.size(); while (abs(ng - ok) > 1) { int mid = ok + (ng - ok) / 2; if (v[mid] >= k) ok = mid; else ng = mid; } return ok; }
 
 void solve() {
