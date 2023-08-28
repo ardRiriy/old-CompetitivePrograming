6c11fd9a68a418f5cf3fbe30e577a34335aff2f4
ardririy <hynnkgw27@gmail.com>
2023-08-28 22:54:26 +0900

    EDPC-Cまで通した

---
+++ EDPC/3.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#define int long long
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define revrep(i, n) for (int i = (int)(n); i >= 0; i--)
+#define itrep(itr, stl) for(auto itr = stl.begin(); itr != stl.end(); itr++)
+#define Vec2D(type, n, m, val) vector<vector<type>>(n, vector<type>(m, val))
+#define print(x) cout << x << endl
+const int INF = LLONG_MAX;
+const int N_INF = LLONG_MIN;
+using namespace std;
+class UnionFind { 
+    private: map<int, int> uf;
+    public:
+    int root(int n) { if (uf.find(n) == uf.end()) uf[n] = -1; if (uf[n] < 0) return n; else return uf[n] = root(uf[n]); }
+    bool connected(int a, int b) { return root(a) == root(b); }
+    void marge(int a, int b) { int root_a = root(a); int root_b = root(b); if (root_a != root_b) { if (uf[root_a] > uf[root_b]) swap(root_a, root_b); uf[root_a] += uf[root_b]; uf[root_b] = root_a; }}
+    int size(int n) { return -uf[root(n)]; }
+};
+bool chmin(int &a, int b) { if (a > b) { a = b; return true; } return false; }
+bool chmax(int &a, int b) { if (a < b) { a = b; return true; } return false; }
+int power(int x, int n) { int result = 1; while(n > 0){ if((n & 1) == 1){ result *= x; } x *= x; n >>= 1; } return result; } /*x^nを計算*/
+int b_search(vector<int>& v, int k) { int ng = -1, ok = v.size(); while (abs(ng - ok) > 1) { int mid = ok + (ng - ok) / 2; if (v[mid] >= k) ok = mid; else ng = mid; } return ok; }
+
+void solve() {
+    // hogehoge
+    int n;
+    cin >> n;
+    auto a = Vec2D(int, n, 3, 0);
+    rep(i, n) cin >> a[i][0] >> a[i][1] >> a[i][2];
+
+    auto dp = Vec2D(int, n+1, 3, N_INF);
+    rep(i, 3) dp[0][i] = 0;
+    rep(i, n){
+        rep(j, 3){
+            if(dp[i][j] == N_INF) continue;
+            rep(k, 3){
+                if(j == k) continue;
+                chmax(dp[i+1][k], dp[i][j] + a[i][k]);
+            }
+        }
+    }
+    print(max(dp[n][0], max(dp[n][1], dp[n][2])));
+}
+
+signed main() {
+    std::cout << std::fixed;
+    std::cout << std::setprecision(20);
+    std::cin.tie(0)->sync_with_stdio(0);
+    int times = 1;
+    // cin >> times;
+    while (times--) solve();
+    return 0;
+}
