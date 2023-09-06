c089f3e024111c5ec02e960662bab598e9edb49f
ardririy <hynnkgw27@gmail.com>
2023-09-06 12:54:47 +0900

    コードの整形と微調整をするなど

--- AHC/AHC023/main.cpp
+++ AHC/AHC023/main.cpp
@@ -6,29 +6,42 @@ using namespace std;
 #define print(x) cout << x << endl
 const int INF = LLONG_MAX;
 const int N_INF = LLONG_MIN;
-const int LMT = 2000; // ミリ秒指定
+const int LMT = 2000;  // ミリ秒指定
 
-
-bool chmin(int &a, int b) { if (a > b) { a = b; return true; } return false; }
-bool chmax(int &a, int b) { if (a < b) { a = b; return true; } return false; }
+bool chmin(int &a, int b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+bool chmax(int &a, int b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
 
 int h = 20, w = 20;
 int t, enter;
-int dx[4] = {0, -1, 0 , 1};
+int dx[4] = {0, -1, 0, 1};
 int dy[4] = {-1, 0, 1, 0};
 
-vector<vector<int>> south_water_route(h-1, vector<int>(w)); // そのマスの座標(i, j)の下に水路があるかどうか
-vector<vector<int>> east_water_route(h, vector<int>(w-1)); //そのマスの右に水路があるかどうか
+vector<vector<int>> south_water_route(
+    h - 1, vector<int>(w));  // そのマスの座標(i, j)の下に水路があるかどうか
+vector<vector<int>> east_water_route(
+    h, vector<int>(w - 1));  // そのマスの右に水路があるかどうか
 vector<vector<int>> board(h, vector<int>(w, -1));
 vector<vector<int>> depth(h, vector<int>(w, INF));
 vector<vector<int>> sd;
 
-struct Pos{
+struct Pos {
     int h;
     int w;
 };
 
-struct Plan{
+struct Plan {
     int i;
     Pos p;
     int s;
@@ -36,66 +49,58 @@ struct Plan{
 
 // board[i][j] -> Edge[i * w + j]と対応
 class LowLink {
-    typedef vector<vector<int>> Edge; // i番目の頂点と隣接なブロックの列
-    vector<bool> visited; // 訪問したかの管理
-    vector<int> order; //順序
-    vector<int> low; //low[親] < low[子]ならブリッジとなる．そうなんだ
-    public:
-        Edge E; // 与えられる木
-        vector<int> articulation_point; // 関節点
-        vector<pair<int, int>> bridge;
-    private:
-        void dfs(int now, int parent, int& count){
-            visited[now] = true;
-            order[now] = count;
-            low[now] = order[now];
-            count++;
-            bool is_art = false; // 関節点かどうかを保存
-            int son_count = 0;
-            for(auto next: E[now]){
-                if(!visited[next]){
-                    son_count++;
-                    dfs(next, now, count);
-                    if(next != parent){
-                        low[now] = min(low[now], low[next]);
-                    }
-                    if(parent != -1 && order[now] <= low[next]){
-                        is_art = true;
-                    }
-                    if(order[now] < low[next]){
-                        bridge.push_back({min(now, next), max(now, next)});
-                    }
-                }else{
-                    if(next != parent){
-                        low[now] = min(low[now], order[next]);
-                    }
+    typedef vector<vector<int>> Edge;  // i番目の頂点と隣接なブロックの列
+    vector<bool> visited;              // 訪問したかの管理
+    vector<int> order;                 // 順序
+    vector<int> low;  // low[親] < low[子]ならブリッジとなる．そうなんだ
+   public:
+    Edge E;                          // 与えられる木
+    vector<int> articulation_point;  // 関節点
+    vector<pair<int, int>> bridge;
+
+   private:
+    void dfs(int now, int parent, int &count) {
+        visited[now] = true;
+        order[now] = count;
+        low[now] = order[now];
+        count++;
+        bool is_art = false;  // 関節点かどうかを保存
+        int son_count = 0;
+        for (auto next : E[now]) {
+            if (!visited[next]) {
+                son_count++;
+                dfs(next, now, count);
+                if (next != parent) {
+                    low[now] = min(low[now], low[next]);
+                }
+                if (parent != -1 && order[now] <= low[next]) {
+                    is_art = true;
+                }
+                if (order[now] < low[next]) {
+                    bridge.push_back({min(now, next), max(now, next)});
+                }
+            } else {
+                if (next != parent) {
+                    low[now] = min(low[now], order[next]);
                 }
             }
-            if(parent == -1 && son_count >= 2){
-                is_art = true;
-            }
-            if(is_art){
-                articulation_point.push_back(now);
-            }
         }
-    public:
-    /**
-    * 渡すのは隣接リスト形式のグラフ。辺の長さは受け付けない.
-    *
-    * \param e
-    */
-    LowLink(const Edge& e) {
+        if (parent == -1 && son_count >= 2) {
+            is_art = true;
+        }
+        if (is_art) {
+            articulation_point.push_back(now);
+        }
+    }
+
+   public:
+    LowLink(const Edge &e) {
         E = e;
         visited.assign(E.size(), 0);
         order.assign(E.size(), 0);
         low.assign(E.size(), 0);
     }
-    /**
-    * 計算の際に呼び出す。各連結成分ごとにDFSしている。.
-    *
-    */
     void build() {
-        // 既存の結果と状態をクリア
         visited.assign(E.size(), 0);
         order.assign(E.size(), 0);
         low.assign(E.size(), 0);
@@ -104,8 +109,7 @@ class LowLink {
 
         int count = 0;
         for (int i = 0; i < E.size(); i++) {
-            if (!visited[i])
-                dfs(i, -1, count);
+            if (!visited[i]) dfs(i, -1, count);
         }
     }
 
@@ -120,7 +124,37 @@ class LowLink {
     }
 };
 
-bool is_through(Pos p, int r){
+struct Edge {
+    int to;
+    int cost;
+};
+
+using Graph = std::vector<std::vector<Edge>>;
+using Pair = std::pair<long long, int>;
+
+void Dijkstra(const Graph &graph, std::vector<long long> &distances, int startIndex) {
+    std::priority_queue<Pair, std::vector<Pair>, std::greater<Pair>> q;
+    q.emplace((distances[startIndex] = 0), startIndex);
+
+    while (!q.empty()) {
+        const long long distance = q.top().first;
+        const int from = q.top().second;
+        q.pop();
+
+        if (distances[from] < distance) {
+            continue;
+        }
+
+        for (const auto &edge : graph[from]) {
+            const long long d = (distances[from] + edge.cost);
+
+            if (d < distances[edge.to]) {
+                q.emplace((distances[edge.to] = d), edge.to);
+            }
+        }
+    }
+}
+bool is_through(Pos p, int r) {
     int new_h = p.h + dy[r];
     int new_w = p.w + dx[r];
 
@@ -129,11 +163,11 @@ bool is_through(Pos p, int r){
     }
 
     // 水路のチェック
-    if (r % 2 == 0) { // 上または下
+    if (r % 2 == 0) {  // 上または下
         if (south_water_route[min(p.h, new_h)][p.w] != 0) {
             return false;
         }
-    } else { // 左または右
+    } else {  // 左または右
         if (east_water_route[p.h][min(p.w, new_w)] != 0) {
             return false;
         }
@@ -141,20 +175,20 @@ bool is_through(Pos p, int r){
     return true;
 }
 
-bool is_reachable_to_enter(Pos p, vector<vector<int>> &bd){
+bool is_reachable_to_enter(Pos p, vector<vector<int>> &bd) {
     queue<Pos> que;
     que.push(p);
     vector<vector<bool>> is_checked(h, vector<bool>(w, false));
     is_checked[p.h][p.w] = true;
-    while(!que.empty()){
+    while (!que.empty()) {
         auto now = que.front();
         que.pop();
-        if(now.h == enter && now.w == 0) return true;
-        rep(i, 4){
+        rep(i, 4) {
             Pos next;
             next.h = now.h + dy[i], next.w = now.w + dx[i];
-            if(is_through(now, i) && !is_checked[next.h][next.w] ){
-                if(bd[next.h][next.w] == -1){
+            if (is_through(now, i) && !is_checked[next.h][next.w]) {
+                if (bd[next.h][next.w] == -1) {
+                    if (next.h == enter && next.w == 0) return true;
                     is_checked[next.h][next.w] = true;
                     que.push(next);
                 }
@@ -164,36 +198,36 @@ bool is_reachable_to_enter(Pos p, vector<vector<int>> &bd){
     return false;
 }
 
-vector<vector<int>> make_linked_list(vector<vector<int>> &bd){
+vector<vector<int>> make_linked_list(vector<vector<int>> &bd) {
     vector<vector<int>> res(h * w);
-    rep(i,h){
-        rep(j, w){
-            rep(r, 4){
-                if(!is_through({i, j}, r)) continue;
+    rep(i, h) {
+        rep(j, w) {
+            rep(r, 4) {
+                if (!is_through({i, j}, r)) continue;
                 int new_h = i + dy[r];
                 int new_w = j + dx[r];
-                if(bd[new_h][new_w] == -1) res[i * w + j].push_back(new_h * w + new_w);
+                if (bd[new_h][new_w] == -1)
+                    res[i * w + j].push_back(new_h * w + new_w);
             }
         }
     }
     return res;
 }
 
-
-void calc_depth(Pos start, vector<vector<int>>&bd){
+void calc_depth(Pos start, vector<vector<int>> &bd) {
     queue<pair<Pos, int>> que;
     fill(depth.begin(), depth.end(), vector<int>(w, INF));
     int now = 0;
     depth[start.h][start.w] = now;
     que.push({start, now});
 
-    while(!que.empty()){
+    while (!que.empty()) {
         auto a = que.front();
         que.pop();
         Pos p = a.first;
         int n = a.second;
-        rep(i, 4){
-            if(!is_through(p, i)) continue;
+        rep(i, 4) {
+            if (!is_through(p, i)) continue;
             int new_h = p.h + dy[i];
             int new_w = p.w + dx[i];
 
@@ -210,17 +244,19 @@ void calc_depth(Pos start, vector<vector<int>>&bd){
     }
 }
 
-bool is_placable(Pos p, vector<int> &v, int proceed_day, vector<vector<int>> &bd){
-    if(bd[p.h][p.w] != -1 || depth[p.h][p.w] == INF || (p.h == enter && p.w ==  0)) return false;
+bool is_placable(Pos p, vector<int> &v, int proceed_day, vector<vector<int>> &bd) {
+    if (bd[p.h][p.w] != -1 || depth[p.h][p.w] == INF ||
+        (p.h == enter && p.w == 0))
+        return false;
 
-    rep(i, 4){
-        if(!is_through(p, i)) continue;
+    rep(i, 4) {
+        if (!is_through(p, i)) continue;
         Pos next;
-        next.h = p.h + dy[i], next.w =p.w + dx[i];
-        if(bd[next.h][next.w] == -1) continue;
-        if(proceed_day > sd[bd[next.h][next.w]-1][1]){
+        next.h = p.h + dy[i], next.w = p.w + dx[i];
+        if (bd[next.h][next.w] == -1) continue;
+        if (proceed_day > sd[bd[next.h][next.w] - 1][1]) {
             bd[p.h][p.w] = 100;
-            if(!is_reachable_to_enter(next, board)) {
+            if (!is_reachable_to_enter(next, board)) {
                 bd[p.h][p.w] = -1;
                 return false;
             }
@@ -228,37 +264,39 @@ bool is_placable(Pos p, vector<int> &v, int proceed_day, vector<vector<int>> &bd
         }
     }
 
-    for(auto i: v) if(p.h * w + p.w == i) return false;
+    for (auto i : v)
+        if (p.h * w + p.w == i) return false;
     return true;
 }
 
-int manhattan_distance_from_wall(Pos p){
-    return pow(enter - p.h, 2) + pow(p.w , 2);
+int manhattan_distance_from_wall(Pos p) {
+    return pow(enter - p.h, 2) + pow(p.w, 2);
 }
 
-
-int calcu_value(int crop_num, Pos p, vector<vector<int>> bd){
+int calcu_value(int crop_num, Pos p, vector<vector<int>> bd) {
     int proceed_day = sd[crop_num - 1][1] - sd[crop_num - 1][0];
     int value = 0;
 
     // 小さい数字は手前に入ってほしいという気持ちをこめている
-    if(proceed_day <= 10) value += pow(depth[p.h][p.w], 2);
+    if (proceed_day < 10) value += pow(depth[p.h][p.w], 2);
 
-    rep(i, 4){
-        if(is_through(p, i)){
+    rep(i, 4) {
+        if (is_through(p, i)) {
             int nh = p.h + dy[i], nw = p.w + dx[i];
-            if(bd[nh][nw] == -1) value += proceed_day;
-            else value += abs(sd[crop_num - 1][1] - sd[bd[nh][nw] - 1][1]);
+            if (bd[nh][nw] == -1)
+                value += proceed_day;
+            else
+                value += abs(sd[crop_num - 1][1] - sd[bd[nh][nw] - 1][1]);
         }
     }
     return value;
 }
 
 /*主に作物の取り除き時に利用*/
-void add_lowlink_edge(Pos p, LowLink &ll, vector<vector<int>> &bd){
-    rep(i, 4){
+void add_lowlink_edge(Pos p, LowLink &ll, vector<vector<int>> &bd) {
+    rep(i, 4) {
         int nh = p.h + dy[i], nw = p.w + dx[i];
-        if(is_through(p, i) && bd[nh][nw] == -1){
+        if (is_through(p, i) && bd[nh][nw] == -1) {
             ll.addEdge(p.h * w + p.w, nh * w + nw);
         }
     }
@@ -266,10 +304,10 @@ void add_lowlink_edge(Pos p, LowLink &ll, vector<vector<int>> &bd){
 }
 
 /*主に植付時に利用*/
-void remove_lowlink_edge(Pos p, LowLink &ll, vector<vector<int>> &bd){
-    rep(i, 4){
+void remove_lowlink_edge(Pos p, LowLink &ll, vector<vector<int>> &bd) {
+    rep(i, 4) {
         int nh = p.h + dy[i], nw = p.w + dx[i];
-        if(is_through(p, i) && bd[nh][nw] == -1){
+        if (is_through(p, i) && bd[nh][nw] == -1) {
             ll.removeEdge(p.h * w + p.w, nh * w + nw);
         }
     }
@@ -277,13 +315,13 @@ void remove_lowlink_edge(Pos p, LowLink &ll, vector<vector<int>> &bd){
 }
 void solve() {
     // hogehoge
-    std::chrono::system_clock::time_point  start, end;
+    std::chrono::system_clock::time_point start, end;
     start = std::chrono::system_clock::now();
     cin >> t >> h >> w >> enter;
 
     vector<vector<bool>> isChecked(h, vector<bool>(w, false));
 
-    rep(i, h-1){
+    rep(i, h - 1) {
         string s;
         cin >> s;
         rep(j, w) south_water_route[i][j] = s[j] - '0';
@@ -291,76 +329,77 @@ void solve() {
     rep(i, h) {
         string s;
         cin >> s;
-        rep(j, w-1) east_water_route[i][j] = s[j] - '0';
+        rep(j, w - 1) east_water_route[i][j] = s[j] - '0';
     }
 
     int k;
     cin >> k;
     map<int, vector<pair<int, int>>> data;
     sd.resize(k, vector<int>(2));
-    rep(i, k){
+    rep(i, k) {
         cin >> sd[i][0] >> sd[i][1];
-        data[sd[i][0]].push_back({sd[i][1], i+1});
+        data[sd[i][0]].push_back({sd[i][1], i + 1});
     }
-    
+
     board.resize(h, vector<int>(w, -1));
     calc_depth({enter, 0}, board);
 
-    rep(i, t) sort(data[i+1].begin(), data[i+1].end());
+    rep(i, t) sort(data[i + 1].begin(), data[i + 1].end());
     vector<Plan> ans;
 
-    auto g = make_linked_list(board);    
+    auto g = make_linked_list(board);
     LowLink lowlink(g);
     lowlink.build();
 
-    for(int month = 1; month <= t;month++){
+    for (int month = 1; month <= t; month++) {
         // 通常植付フェーズ
-        for(auto itr = data[month].rbegin(); itr != data[month].rend(); itr++){
+        for (auto itr = data[month].rbegin(); itr != data[month].rend(); itr++) {
             auto c = *itr;
 
             Pos option = {-1, -1};
             int perf = 1e5;
-            rep(i, h){
-                rep(j, w){
-                    if(is_placable({i, j}, lowlink.articulation_point, c.first, board)){
+            rep(i, h) {
+                rep(j, w) {
+                    if (is_placable({i, j}, lowlink.articulation_point, c.first,
+                                    board)) {
                         int t_v = calcu_value(c.second, {i, j}, board);
-                        if(chmin(perf, t_v)){
+                        if (chmin(perf, t_v)) {
                             option = {i, j};
-                        }else if(perf == t_v){
-                            if(depth[i][j] < depth[option.h][option.w]){
+                        } else if (perf == t_v) {
+                            if (depth[i][j] < depth[option.h][option.w]) {
                                 option = {i, j};
                             }
                         }
-                    }         
+                    }
                 }
             }
-            if(option.h != -1){
+            if (option.h != -1) {
                 board[option.h][option.w] = c.second;
                 remove_lowlink_edge(option, lowlink, board);
                 ans.push_back({board[option.h][option.w], option, month});
             }
         }
 
-        //収穫
+        // 収穫
         queue<Pos> que;
         que.push({enter, 0});
 
         fill(isChecked.begin(), isChecked.end(), vector<bool>(w, false));
-        while(!que.empty()){
+        while (!que.empty()) {
             auto p = que.front();
             que.pop();
-            rep(i, 4){
-                if(!is_through(p, i)) continue;
+            rep(i, 4) {
+                if (!is_through(p, i)) continue;
 
                 int new_h = p.h + dy[i];
                 int new_w = p.w + dx[i];
-                if(isChecked[new_h][new_w]) continue;
+                if (isChecked[new_h][new_w]) continue;
                 // boardのチェック
                 int crop_num = board[new_h][new_w];
                 if (crop_num == -1) {
                     que.push({new_h, new_w});
                     isChecked[new_h][new_w] = true;
-                }else if(sd[crop_num-1][1] <= month){
+                } else if (sd[crop_num - 1][1] <= month) {
                     board[new_h][new_w] = -1;
                     add_lowlink_edge({new_h, new_w}, lowlink, board);
                     que.push({new_h, new_w});
@@ -371,11 +410,9 @@ void solve() {
     }
 
     print(ans.size());
-    rep(i, ans.size()){
-        print(ans[i].i<< " " << ans[i].p.h << " " << ans[i].p.w << " " <<ans[i].s);
+    rep(i, ans.size()) {
+        print(ans[i].i << " " << ans[i].p.h << " " << ans[i].p.w << " " << ans[i].s);
     }
-
-
 }
 
 signed main() {
