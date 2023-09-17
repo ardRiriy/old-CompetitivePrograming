e0d71aa8329de9d747274cfbaf7726a6163d9d33
ardririy <hynnkgw27@gmail.com>
2023-09-17 17:25:31 +0900

    Hardをすすめた

---
+++ ABC/C/050.cpp
@@ -0,0 +1,114 @@
+#include <bits/stdc++.h>
+#define int long long
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define revrep(i, n) for (int i = (int)(n); i >= 0; i--)
+#define itrep(itr, stl) for(auto itr = stl.begin(); itr != stl.end(); itr++)
+#define Vec2D(type, n, m, val) vector<vector<type>>(n, vector<type>(m, val))
+#define print(x) cout << x << endl
+#define all(a) a.begin(), a.end()
+const int INF = LLONG_MAX;
+const int N_INF = LLONG_MIN;
+using namespace std;
+class UnionFind {
+private: map<int, int> uf;
+public:
+    int root(int n) { if (uf.find(n) == uf.end()) uf[n] = -1; if (uf[n] < 0) return n; else return uf[n] = root(uf[n]); }
+    bool connected(int a, int b) { return root(a) == root(b); }
+    void marge(int a, int b) { int root_a = root(a); int root_b = root(b); if (root_a != root_b) { if (uf[root_a] > uf[root_b]) swap(root_a, root_b); uf[root_a] += uf[root_b]; uf[root_b] = root_a; }}
+    int size(int n) { return -uf[root(n)]; }
+};
+bool chmin(int &a, int b) { if (a > b) { a = b; return true; } return false; }
+bool chmax(int &a, int b) { if (a < b) { a = b; return true; } return false; }
+int power(int x, int n) { int result = 1; while(n > 0){ if((n & 1) == 1){ result *= x; } x *= x; n >>= 1; } return result; } /*x^nを計算*/
+int b_search(vector<int>& v, int k) { int ng = -1, ok = v.size(); while (abs(ng - ok) > 1) { int mid = ok + (ng - ok) / 2; if (v[mid] >= k) ok = mid; else ng = mid; } return ok; }
+
+
+template<int MOD>
+struct ModInt {
+    int value;
+
+    ModInt() : value(0) {}
+    ModInt(int value) : value(value % MOD) {
+        if (this->value < 0) this->value += MOD;
+    }
+
+    ModInt operator+(const ModInt& other) const {
+        return ModInt((value + other.value) % MOD);
+    }
+
+    ModInt operator-(const ModInt& other) const {
+        return ModInt((value - other.value + MOD) % MOD);
+    }
+
+    ModInt operator*(const ModInt& other) const {
+        return ModInt((1LL * value * other.value) % MOD);
+    }
+
+    ModInt operator/(const ModInt& other) const {
+        return *this * other.inv();
+    }
+
+    ModInt inv() const {
+        int a = value, b = MOD, u = 1, v = 0;
+        while (b) {
+            int t = a / b;
+            a -= t * b; std::swap(a, b);
+            u -= t * v; std::swap(u, v);
+        }
+        return ModInt(u);
+    }
+};
+
+
+
+void solve() {
+    int n;
+    cin >> n;
+    map<int, int> mp;
+    rep(i, n){
+        int a;
+        cin >> a;
+        mp[a]++;
+    }
+    if(n % 2 == 0){
+        for(int i = 1; i <= n / 2; i += 2){
+            if(mp[i] != 2){
+                print(0);
+                return;
+            }
+        }
+    }else{
+        if(mp[0] != 1){
+            print(0);
+            return;
+        }
+        for(int i = 2; i <= n / 2 + 1; i += 2){
+            if(mp[i] != 2){
+                print(0);
+                return;
+            }
+        }
+    }
+    if(n == 1){
+        print(1);
+        return;
+    }
+
+    using mint = ModInt<(int)1e9 + 7>;
+    mint ans = 1;
+    rep(i, n / 2){
+        ans = ans * 2;
+    }
+    print(ans.value);
+
+}
+
+signed main() {
+    std::cout << std::fixed;
+    std::cout << std::setprecision(20);
+    std::cin.tie(0)->sync_with_stdio(0);
+    int times = 1;
+    // cin >> times;
+    while (times--) solve();
+    return 0;
+}
