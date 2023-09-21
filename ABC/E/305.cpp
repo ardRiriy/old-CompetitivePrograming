535ab09eda2a5b14b44430e0d7a2385d3ad05142
ardririy <hynnkgw27@gmail.com>
2023-09-21 19:09:16 +0900

    今までわからなかったやつを通せた

--- ABC/E/305.cpp
+++ ABC/E/305.cpp
@@ -1,109 +1,98 @@
 #include <bits/stdc++.h>
 #define int long long
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define revrep(i, n) for (int i = (int)(n); i >= 0; i--)
 #define itrep(itr, stl) for(auto itr = stl.begin(); itr != stl.end(); itr++)
+#define Vec2D(type, n, m, val) vector<vector<type>>(n, vector<type>(m, val))
 #define print(x) cout << x << endl
+#define all(a) a.begin(), a.end()
 const int INF = LLONG_MAX;
 const int N_INF = LLONG_MIN;
 using namespace std;
 class UnionFind { 
-    private: vector<int> uf;
+    private: map<int, int> uf;
     public:
-    UnionFind(int size) : uf(size) { for (int i = 0; i < size; i++) uf[i] = -1; }
-    int root(int n) { if (uf[n] < 0) return n; else return uf[n] = root(uf[n]); }
+    int root(int n) { if (uf.find(n) == uf.end()) uf[n] = -1; if (uf[n] < 0) return n; else return uf[n] = root(uf[n]); }
     bool connected(int a, int b) { return root(a) == root(b); }
     void marge(int a, int b) { int root_a = root(a); int root_b = root(b); if (root_a != root_b) { if (uf[root_a] > uf[root_b]) swap(root_a, root_b); uf[root_a] += uf[root_b]; uf[root_b] = root_a; }}
     int size(int n) { return -uf[root(n)]; }
 };
 bool chmin(int &a, int b) { if (a > b) { a = b; return true; } return false; }
 bool chmax(int &a, int b) { if (a < b) { a = b; return true; } return false; }
-int power(int base, int exponent) {int result = 1;for (int i = 0; i < exponent; i++) result *= base; return result; }
+int power(int x, int n) { int result = 1; while(n > 0){ if((n & 1) == 1){ result *= x; } x *= x; n >>= 1; } return result; } /*x^nを計算*/
 int b_search(vector<int>& v, int k) { int ng = -1, ok = v.size(); while (abs(ng - ok) > 1) { int mid = ok + (ng - ok) / 2; if (v[mid] >= k) ok = mid; else ng = mid; } return ok; }
 
-// 辺の情報
-struct Edge
-{
-	// 行先
-	int to;
-
-	// コスト
-	int cost;
+struct Edge {
+    int to;
+    int cost;
 };
 
-using Graph = std::vector<std::vector<Edge>>;
+using Graph = vector<vector<Edge>>;
 
-// { distance, from }
-using Pair = std::pair<long long, int>;
 
-// ダイクストラ法 (1.1 基本実装)
-// distances は頂点数と同じサイズ, 全要素 INF で初期化しておく
-void Dijkstra(const Graph& graph, std::vector<long long>& distances, int startIndex)
-{
-	// 「現時点での最短距離, 頂点」の順に取り出す priority_queue
-	// デフォルトの priority_queue は降順に取り出すため std::greater を使う
-	std::priority_queue<Pair, std::vector<Pair>, std::greater<Pair>> q;
-	q.emplace((distances[startIndex] = 0), startIndex);
 
-	while (!q.empty())
-	{
-		const long long distance = q.top().first;
-		const int from = q.top().second;
-		q.pop();
+void Dijkstra(Graph &graph, vector<int> &dist, map<int, int> start){
+    auto compare = [](pair<int, int> & a, pair<int, int> & b) {
+        return a.second < b.second;
+    };
+    priority_queue<
+        pair<int, int>,
+        vector<pair<int, int>>,
+        decltype(compare)
+    > pq{compare}; // {行先, そこまでの最大HP候補}
 
-		// 最短距離でなければ処理しない
-		if (distances[from] < distance)
-		{
-			continue;
-		}
+    itrep(itr, start){
+        pq.push({itr->first, itr->second});
+    }
 
-		// 現在の頂点からの各辺について
-		for (const auto& edge : graph[from])
-		{
-			// to までの新しい距離
-			const long long d = (distances[from] + edge.cost);
+    while(!pq.empty()){
+        auto p = pq.top();
+        pq.pop();
+        if(dist[p.first] != INF) continue;
+        dist[p.first] = p.second;
+        rep(i, graph[p.first].size()){
+            if(dist[graph[p.first][i].to] == INF){
+                pq.push({graph[p.first][i].to, p.second - 1});
+            }
+        }
+    }
 
-			// d が現在の記録より小さければ更新
-			if (d < distances[edge.to])
-			{
-				q.emplace((distances[edge.to] = d), edge.to);
-			}
-		}
-	}
 }
 
 void solve() {
     // hogehoge
     int n, m, k;
     cin >> n >> m >> k;
+
     Graph g(n);
-    rep(i, m){
-        int u, v;
-        cin >> u >> v;
-        u--;v--;
-        g[u].push_back({v, 1});
-        g[v].push_back({u, 1});
+    rep(_, m){
+        int a, b;
+        cin >> a >> b;
+        a--;
+        b--;
+        g[a].push_back({b, 1});
+        g[b].push_back({a, 1});
     }
 
-    vector<bool> ans(n, false);
-    int sum = 0;
-    rep(i, k){
+    vector<int> d(n, INF);
+    map<int, int> start;
+    rep(_, k){
         int p, h;
         cin >> p >> h;
-        p--;
-        vector<int> dist(n, INF);
-        Dijkstra(g, dist, p);
-        rep(j, n){
-            if(ans[j])continue;
-            if(dist[j] <= h) {
-                sum++;
-                ans[j] = true;
-            }
-        }
+        start[p - 1] = h;
     }
 
-    print(sum);
-    rep(i, n)if(ans[i]) cout << i+1 << " ";
-    print("");
+    Dijkstra(g, d, start);
+
+    vector<int> ans;
+    rep(i, n){
+        if(d[i] >= 0 && d[i] != INF) ans.push_back(i+1);
+    }
+
+    print(ans.size());
+    rep(i, ans.size()){
+        cout << ans[i] << (i == ans.size()-1?"\n":" ");
+    }
 }
 
 signed main() {
